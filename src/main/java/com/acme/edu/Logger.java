ea8c6832c725e95e0acb3fcb93067a3a970e6ca5
dy.samsonov <dy.samsonov@jet.msk.su>
2017-09-27 13:57:36 +0300

    test

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,38 @@
+
 package com.acme.edu;
+
+import static com.acme.edu.Logger.logString;
 
 public class Logger {
     public static void log(int message) {
         System.out.println("primitive: " + message);
     }
 
+    public static void logString(String message){
+        System.out.println(message);
+    }
+
     public static void log(byte message) {
         System.out.println("primitive: " + message);
+    }
+
+    public static void main(String[] args) {
+        logString("HW!!");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        logString("HW!!");
+    }
+    public void m() {
+
+    }
+}
+
+class Rt{
+    public static void main(String[] args) {
+        byte b;
+        System.out.println(b);
     }
 }
