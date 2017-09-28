0fe5c78646be0e35de6029ec27cec696feb7aba9
Alekto <alektotaras@yandex.ru>
2017-09-28 15:53:07 +0300

    Iteration02

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -18,18 +18,72 @@ public class Logger {
     public static int globalState = 0;
     public int instanceState = 0;
 
+    public static String etalonString = null;
+    public static int etalonCounter = 0;
+
+    public static int sum = 0;
+    public static boolean isSumExist=false;
     public static String ClassName = "";
     public static final String PRIM= "primitive: ";
 
+    public static void printString(String string, int etalonCounter)
+    {
+        if(etalonCounter>1)
+            System.out.println(string + " (x" +etalonCounter+ ")");
+        else if(etalonCounter > 0)
+            System.out.println(string);
+
+    }
+    /*
+    public static void printValue(int sum)
+    {
+        if(etalonCounter>1)
+            System.out.println(string + " (x" +etalonCounter+ ")");
+        else if(etalonCounter > 0)
+            System.out.println(string);
+    }*/
     public static void log(byte foramlMessage) {
-        String etalon = (new Byte(foramlMessage)).getClass().toString();
+        /*String etalon = (new Byte(foramlMessage)).getClass().toString();
         if(!ClassName.equals(etalon)) {
             ClassName = etalon;
             System.out.println(PRIM+"\r\n" + foramlMessage);
         }else {
             System.out.println(foramlMessage);
+        }*/
+        if(etalonString!=null)//была строка - выводим в out
+        {
+            if(foramlMessage == Byte.MAX_VALUE)
+            {
+                printString(etalonString, etalonCounter);
+                printMV(Byte.MAX_VALUE);
+            }else {
+                printString(etalonString, etalonCounter);
+                etalonString = null;
+                etalonCounter = 0;
+                sum = foramlMessage;
+                isSumExist = true;
+            }
+        }
+        else {//не было строки
+            if(foramlMessage == Byte.MAX_VALUE)
+            {
+                if(isSumExist) {
+                    System.out.println(sum + "");
+                }
+                printMV(Byte.MAX_VALUE);
+                sum=0;
+                isSumExist = false;
+            }
+            else{
+                sum+=foramlMessage;
+            }
         }
     }
+
+    private static void printMV(int maxValue) {
+        System.out.println(maxValue+"");
+    }
+
     public static void log(int foramlMessage) {
         /*String etalon = (new Integer(foramlMessage)).getClass().toString();
         if(!ClassName.equals(etalon)) {
@@ -38,7 +92,34 @@ public class Logger {
         }else {
             System.out.println(foramlMessage);
         }*/
-        System.out.print(PRIM + foramlMessage+"\r\n");
+        if(etalonString!=null)//была строка - выводим в out
+        {
+            if(foramlMessage == Integer.MAX_VALUE)
+            {
+                printString(etalonString, etalonCounter);
+                printMV(Integer.MAX_VALUE);
+            }else {
+                printString(etalonString, etalonCounter);
+                etalonString = null;
+                etalonCounter = 0;
+                sum = foramlMessage;
+                isSumExist = true;
+            }
+        }
+        else {//не было строки
+            if(foramlMessage == Integer.MAX_VALUE)
+            {
+                if(isSumExist) {
+                    System.out.println(sum + "");
+                }
+                printMV(Integer.MAX_VALUE);
+                sum=0;
+                isSumExist = false;
+            }
+            else{
+                sum+=foramlMessage;
+            }
+        }
     }
     public static void log(char foramlMessage) {
         String etalon = (new Character(foramlMessage)).getClass().toString();
@@ -50,13 +131,46 @@ public class Logger {
         }
     }
     public static void log(String foramlMessage) {
-        String etalon = (new String(foramlMessage)).getClass().toString();
+        /*String etalon = (new String(foramlMessage)).getClass().toString();
         if(!ClassName.equals(etalon)) {
             ClassName = etalon;
             System.out.println("string: \r\n" + foramlMessage);
         }else {
             System.out.println(foramlMessage);
+        }*/
+
+
+        if (sum == -1)
+        {
+            //System.out.println(foramlMessage);
+            etalonString = foramlMessage;
+            etalonCounter = 1;
+            sum = 0;
         }
+        else if(sum == 0)
+        {
+            if(etalonString == null)
+            {
+                etalonString = foramlMessage;
+                etalonCounter++;
+            }else if(!foramlMessage.equals(etalonString))
+            {
+                printString(etalonString,etalonCounter);
+                etalonString = foramlMessage;
+                etalonCounter = 1;
+            }else
+            {
+                etalonCounter++;
+            }
+
+        }
+        else {
+            System.out.println(sum);
+            etalonString=foramlMessage;
+            etalonCounter = 1;
+            //System.out.println(foramlMessage);
+        }
+
     }
 
     public static void log(Object obj) {
@@ -98,11 +212,11 @@ public class Logger {
 
 
     public static void main(String... args) {
-        int local = 0;
-        //kdjfhgkjfgh
-        log(1);
-
-        System.out.println(new String("").getClass().toString());
+        Logger.log("str 1");
+        Logger.log(1);
+        Logger.log(2);
+        Logger.log("str 2");
+        Logger.log(0);
 //        Main.main();
 //        Main.counter = 1;
     }
